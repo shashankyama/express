b4415a7132adb212fc4465fe5c9f63cf58b729d7
bunny yama <yamashashank@gmail.com>
2023-07-21 14:40:23 -0500

    promises

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,20 @@
-console.log('Express Tutorial')
+const express = require("express");
+const app = express();
+const path = require("path");
+
+//setup static and middleware
+app.use(express.static("./public"));
+
+// app.get("/", (req, res) => {
+//   res.sendFile(path.resolve(__dirname, "./navbar-app/index.html"));
+// adding to static assets
+// server site rendering
+// });
+
+app.all("*", (req, res) => {
+  res.status(404).send("Cannot find Resources");
+});
+
+app.listen(5000, () => {
+  console.log("server is listening on port 5000");
+});
